d6953209775a00762e43e17e7dae5b7cbd64b748
Oleg Parashchenko <olpa@uucode.com>
2016-07-17 20:58:39 +0200

    Command line parsing separated.

--- w09/httpserver.cpp
+++ w09/httpserver.cpp
@@ -3,39 +3,53 @@
 #include <unistd.h>
 #include <assert.h>
 
-int main(int argc, char **argv) {
+struct ServerOptions {
   bool stay_foreground;
+  bool config_ok;
   std::string ip_address;
   unsigned int port;
   std::string root;
+};
+
+ServerOptions parse_cmdline(int argc, char **argv) {
+  ServerOptions opt;
   int c;
   while ((c = getopt(argc, argv, "Fh:p:d:")) != -1) {
     switch (c) {
       case 'F':
-        stay_foreground = true;
+        opt.stay_foreground = true;
         std::cout << "StayForeground=true" << std::endl;
         break;
       case 'h':
-        ip_address = optarg;
-        std::cout << "IP=" << ip_address << std::endl;
+        opt.ip_address = optarg;
+        std::cout << "IP=" << opt.ip_address << std::endl;
         break;
       case 'p':
-        port = std::stoi(optarg);
-        std::cout << "port=" << port << std::endl;
+        opt.port = std::stoi(optarg);
+        std::cout << "port=" << opt.port << std::endl;
         break;
       case 'd':
-        root = optarg;
-        std::cout << "directory=" << root << std::endl;
+        opt.root = optarg;
+        std::cout << "directory=" << opt.root << std::endl;
         break;
       case ':':
         std::cerr << "Option -" << static_cast<char>(optopt) << " requires an operand" << std::endl;
-        return 1;
+        return opt;
       case '?':
         std::cerr << "Unrecognized option -" << static_cast<char>(optopt) << std::endl;
-        return 1;
+        return opt;
       default:
         std::cerr << "Internal error parsing options" << std::endl;
-        return 1;
+        return opt;
     }
+  }
+  opt.config_ok = true;
+  return opt;
+}
+
+int main(int argc, char **argv) {
+  ServerOptions opt = parse_cmdline(argc, argv);
+  if (! opt.config_ok) {
+    return 1;                                              // exit
   }
 }
